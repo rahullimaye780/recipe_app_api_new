b27be75a148b8d965c9bb59e1bacbb46b2f4b2d2
rahullimaye780 <rahul_limaye780@yahoo.com>
2026-04-05 17:22:17 +0530

    db connection and model

--- app/core/models.py
+++ app/core/models.py
@@ -15,8 +15,21 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        #user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    #rahul_limaye780@example.com  pass -rahul12345
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
